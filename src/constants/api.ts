59df107210711b3152626e6fce56b01249e89f58
Akshay <akshay@rimba.ai>
2025-05-22 12:32:31 -0700

    Update api.ts

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: "https://vertex-api.rimba.ai/vertex"
+  BASE_URL: "https://app-v1.rimba.ai/vertex"
+  // BASE_URL: "https://vertex-api.rimba.ai/vertex"
   // BASE_URL: "https://fd4c-44-219-29-198.ngrok-free.app/vertex"
 } as const;
