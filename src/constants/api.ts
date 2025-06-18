610ee635c83e97222aadf4af94231be43b191c3e
Ankit Bharti <64502877+ankitcodes001@users.noreply.github.com>
2025-06-18 09:09:38 +0530

    COMMENTED API

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
   // BASE_URL: "http://ec2-13-52-240-31.us-west-1.compute.amazonaws.com/vertex"
-  BASE_URL: "https://vertex-api.rimba.ai/vertex"
+  // BASE_URL: "https://vertex-api.rimba.ai/vertex"
   // BASE_URL: "https://fd4c-44-219-29-198.ngrok-free.app/vertex"
 } as const;
